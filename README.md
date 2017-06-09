7936ed677f9862c7f2758a1b1389592332b5b7bc
Don Pinkster <don@pinkster.me>
2017-06-09 10:24:24 +0200

    preparing for publish

--- README.md
+++ README.md
@@ -12,3 +12,16 @@ def deps do
   [{:airports, "~> 0.1.0"}]
 end
 ```
+
+## Contributing
+
+Before opening a pull request, please open an issue first.
+
+Once we've decided how to move forward with a pull request:
+
+    $ git clone https://github.com/nerds-and-company/airports.git
+    $ cd airports
+    $ mix deps.get
+    $ mix test
+
+Once you've made your additions and `mix test` passes, go ahead and open a PR!
--- mix.exs
+++ mix.exs
@@ -1,6 +1,8 @@
 defmodule Airports.Mixfile do
   use Mix.Project
 
+  @project_url "https://github.com/nerds-and-company/airports"
+
   def project do
     [app: :airports,
      version: "0.1.0",
@@ -10,7 +12,7 @@ defmodule Airports.Mixfile do
      deps: deps(),
      description: description(),
      package: package(),
-     source_url: "https://github.com/nerds-and-company/airports"]
+     source_url: @project_url]
   end
 
   def application do
@@ -19,7 +21,9 @@ defmodule Airports.Mixfile do
 
   defp deps do
     [
-      {:csv, "~> 1.4.2"}
+      {:csv, "~> 1.4.2"},
+
+      {:ex_doc, ">= 0.0.0", only: :dev}
     ]
   end
 
@@ -32,6 +36,6 @@ defmodule Airports.Mixfile do
   defp package do
     [maintainers: ["Don Pinkster"],
      licenses: ["MIT"],
-     links: %{"GitHub" => "https://github.com/nerds-and-company/airports"}]
+     links: %{"GitHub" => @project_url}]
   end
 end
